1168f386493bab55079fa2afe18525ef9d64e4be
oliverfei <ofei006@uottawa.ca>
2015-07-03 14:03:21 -0400

    Edited styles

--- scripts.js
+++ scripts.js
@@ -5,3 +5,9 @@ function highlight(button) {
 function unhighlight(button) {
     button.style.background = "#1a1919";
 }
+
+function scrollAdjust(divID) {
+    var adjust = document.getElementById("header").offsetHeight;
+    var targetY = document.getElementById(divID).getBoundingClientRect().top;
+    window.scrollBy(0, targetY-adjust-100);
+}
